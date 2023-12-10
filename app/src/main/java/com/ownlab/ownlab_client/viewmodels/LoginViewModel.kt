48897f5a72d6cd20153d7e461a57097732dbe5df
enfycius <hexk0131@gmail.com>
2023-12-10 20:45:37 +0900

    fix #13

--- app/src/main/java/com/ownlab/ownlab_client/viewmodels/LoginViewModel.kt
+++ app/src/main/java/com/ownlab/ownlab_client/viewmodels/LoginViewModel.kt
@@ -6,8 +6,10 @@ import com.ownlab.ownlab_client.models.LoginResponse
 import com.ownlab.ownlab_client.repository.AuthRepository
 import com.ownlab.ownlab_client.utils.ApiResponse
 import com.ownlab.ownlab_client.viewmodels.`interface`.CoroutinesErrorHandler
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class LoginViewModel @Inject constructor (private val authRepository: AuthRepository): BaseViewModel() {
     private var _loginResponse = MutableLiveData<ApiResponse<LoginResponse>>()
     val loginResponse = _loginResponse
